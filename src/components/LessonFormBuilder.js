351d92cd645b6c288c864eb54c8dd7c5dad95cf0
Robert Laws <roblaws@me.com>
2021-04-06 16:01:26 +0300

    add tailwind forms and refactor root component

---
+++ src/components/LessonFormBuilder.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LessonFormBuilder = () => {
+  return (
+    <div className='container mx-auto px-4'>
+      <h1 className='text-2xl'>Lesson Form</h1>
+    </div>
+  );
+};
+
+export default LessonFormBuilder;
