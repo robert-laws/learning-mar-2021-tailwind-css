b2ae97db7a8833a164d297de7ba9017df950cd0b
Robert Laws <roblaws@me.com>
2021-04-06 16:09:36 +0300

    add context and hooks for Courses

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/css/tailwind.css';
 import App from './App';
+import CoursesState from './context/courses/CoursesState';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CoursesState>
+      <App />
+    </CoursesState>
   </React.StrictMode>,
   document.getElementById('root')
 );
