f25b07615457db6a6ea69f000e04b6a6133ab92b
Robert Laws <roblaws@me.com>
2021-04-10 19:12:14 +0300

    minor change ot reviewlesson component

--- src/components/ReviewLesson.js
+++ src/components/ReviewLesson.js
@@ -38,8 +38,6 @@ const ReviewLesson = ({ handleUpdateStep }) => {
     return values;
   };
 
-  console.log(lesson);
-
   return (
     <div className='container mx-auto px-4'>
       <h1 className='text-3xl'>Review Lesson</h1>
